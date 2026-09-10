ff67979f9b8fd8b69a1c5ab4d0e4e7ca7feb2c9a
Vishruth Thimmaiah <vishruththimmaiah@gmail.com>
2026-09-10 21:22:21 +0530

    fix: do not print lldb version unless -v is used
    Signed-off-by: Vishruth Thimmaiah <vishruththimmaiah@gmail.com>

--- ocldbg/main.cpp
+++ ocldbg/main.cpp
@@ -32,6 +32,7 @@ struct ProgramArgs {
     bool dry_run = false;
     bool show_wg_bounds = false;
     bool show_help = false;
+    bool show_version = false;
 };
 
 ProgramArgs parse_arguments(int argc, char **argv) {
@@ -46,6 +47,8 @@ ProgramArgs parse_arguments(int argc, char **argv) {
             args.backend_name = argv[++i];
         } else if (arg == "--port" && (i + 1 < argc)) {
             args.dap_port = static_cast<uint16_t>(std::stoi(argv[++i]));
+        } else if (arg == "-v" || arg == "--version") {
+            args.show_version = true;
         } else if (arg == "-h" || arg == "--help") {
             args.show_help = true;
         } else if (!arg.empty() && arg[0] != '-') {
@@ -59,6 +62,19 @@ ProgramArgs parse_arguments(int argc, char **argv) {
     return args;
 }
 
+void print_version() {
+    std::string lldb_version = ocldbg::DebuggerContext::init();
+#ifndef OCLDBG_GIT_COMMIT
+#define OCLDBG_GIT_COMMIT "unknown"
+#endif
+#ifndef OCLDBG_VERSION
+#define OCLDBG_VERSION "0.1.0"
+#endif
+    std::cout << std::format("ocldbg version {} (commit {})\nUsing {}\n", OCLDBG_VERSION,
+                             OCLDBG_GIT_COMMIT, lldb_version);
+    ocldbg::DebuggerContext::terminate();
+}
+
 void print_help() {
     std::cout << std::format(
         "Usage: ocldbg [options] <host_binary> [args...]\n"
@@ -66,7 +82,9 @@ void print_help() {
         "  --dry-run          [TEMP] Test launch and NDRange inference without full execution\n"
         "  --show-wg-bounds   Display inferred work-group bounds and coordinate mapping\n"
         "  --backend <name>   Execution backend (default: cpu)\n"
-        "  --port <port>      Listen on TCP port for DAP client (default: stdio)\n");
+        "  --port <port>      Listen on TCP port for DAP client (default: stdio)\n"
+        "  -v, --version      Display version and build information\n"
+        "  -h, --help         Display this help message\n");
 }
 
 void print_inferred_bounds(const ocldbg::KernelLaunchInfo &info) {
@@ -92,22 +110,24 @@ void print_inferred_bounds(const ocldbg::KernelLaunchInfo &info) {
 } // namespace
 
 int main(int argc, char **argv) {
-    std::string lldb_version = ocldbg::DebuggerContext::init();
-    std::cout << std::format("Using {}\n", lldb_version);
-
     ProgramArgs args = parse_arguments(argc, argv);
+
+    if (args.show_version) {
+        print_version();
+        return 0;
+    }
 
     if (args.show_help) {
         print_help();
-        ocldbg::DebuggerContext::terminate();
         return 0;
     }
 
     if (args.host_binary.empty()) {
         std::cerr << "Usage: ocldbg [options] <host_binary> [args...]\n";
-        ocldbg::DebuggerContext::terminate();
         return 1;
     }
+
+    ocldbg::DebuggerContext::init();
 
     ocldbg::DebuggerContext dbg;
     if (!dbg.launch(args.host_binary, args.host_args)) {
@@ -116,8 +136,9 @@ int main(int argc, char **argv) {
     }
 
     if (dbg.infer_kernel_launch()) {
-        if (args.show_wg_bounds && dbg.kernel_launch_info()) {
-            print_inferred_bounds(*dbg.kernel_launch_info());
+        const auto &launch_info = dbg.kernel_launch_info();
+        if (args.show_wg_bounds && launch_info.has_value()) {
+            print_inferred_bounds(*launch_info);
         }
     } else {
         std::cerr << "[ocldbg] Warning: Could not infer NDRange from kernel call site\n";
